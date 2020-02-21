538f2da052466b3d25789576b0c94d0fcf749299
Michal Frankowski <michal.frankowski@kindredgroup.com>
2020-02-21 16:27:59 +0100

    Fix paths

--- Formula/inspect.rb
+++ Formula/inspect.rb
@@ -4,8 +4,8 @@ class Inspect < Formula
   #url "https://github.com/TheHipbot/weather/raw/master/archive/weather-1.0.0.tar.gz"
   #sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
   url "https://27.io/inspect.zip"
-  sha256 "84b98232e4132b4d7283e6311e372df83de99fec9aeb25557e5f2edc886a19de"
-  version "1.0.5"
+  sha256 "9d42ed95b4f4f344d895436a087eb031afeb15968800b46d1a2ebbfdf5f04787"
+  version "1.0.6"
 
   depends_on "python"
   depends_on "jq"
